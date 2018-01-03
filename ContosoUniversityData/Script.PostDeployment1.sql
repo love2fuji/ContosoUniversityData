239b589973d3c6296178b57dc4ed30cd46470864
xiaoshi WEN <love2fuji@outlook.com>
2018-01-03 10:38:36 +0800

    创建 Web 应用程序和数据模型

---
+++ ContosoUniversityData/Script.PostDeployment1.sql
@@ -0,0 +1,50 @@
+﻿/*
+后期部署脚本模板							
+--------------------------------------------------------------------------------------
+ 此文件包含将附加到生成脚本中的 SQL 语句。		
+ 使用 SQLCMD 语法将文件包含到后期部署脚本中。			
+ 示例:      :r .\myfile.sql								
+ 使用 SQLCMD 语法引用后期部署脚本中的变量。		
+ 示例:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+MERGE INTO Course AS Target 
+USING (VALUES 
+        (1, 'Economics', 3), 
+        (2, 'Literature', 3), 
+        (3, 'Chemistry', 4)
+) 
+AS Source (CourseID, Title, Credits) 
+ON Target.CourseID = Source.CourseID 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (Title, Credits) 
+VALUES (Title, Credits);
+
+MERGE INTO Student AS Target
+USING (VALUES 
+        (1, 'Tibbetts', 'Donnie', '2013-09-01'), 
+        (2, 'Guzman', 'Liza', '2012-01-13'), 
+(3, 'Catlett', 'Phil', '2011-09-03')
+)
+AS Source (StudentID, LastName, FirstName, EnrollmentDate)
+ON Target.StudentID = Source.StudentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (LastName, FirstName, EnrollmentDate)
+VALUES (LastName, FirstName, EnrollmentDate);
+
+MERGE INTO Enrollment AS Target
+USING (VALUES 
+(1, 2.00, 1, 1),
+(2, 3.50, 1, 2),
+(3, 4.00, 2, 3),
+(4, 1.80, 2, 1),
+(5, 3.20, 3, 1),
+(6, 4.00, 3, 2)
+)
+AS Source (EnrollmentID, Grade, CourseID, StudentID)
+ON Target.EnrollmentID = Source.EnrollmentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Grade, CourseID, StudentID)
+VALUES (Grade, CourseID, StudentID);
